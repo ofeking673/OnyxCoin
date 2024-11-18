aa5b50507e461140f082fd9c709bbf2d57a0be7c
ofeking475 <ofeking475@gmail.com>
2024-11-18 10:00:10 +0200

    Added base58 encoding, now creating a public wallet address is complete.

---
+++ Blockchain_Project/Base58.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Point.h"
+#include <vector>
+#include <iostream>;
+
+class Base58
+{
+public:
+	Base58() = default;
+	std::vector<char> vect = {
+		'1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	std::string base58(cpp_int inp);
+	cpp_int mod(cpp_int a, cpp_int m) {
+		return (a % m + m) % m;
+	}
+};
+
