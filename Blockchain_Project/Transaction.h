ae7f4ca07d6910f1ffa43dad80db59b5a46177f8
unknown <neslioz07@gmail.com>
2024-12-24 14:07:25 +0200

    Implemented infrastructure for Transactions, UTXOs and Wallet

--- Blockchain_Project/Transaction.h
+++ Blockchain_Project/Transaction.h
@@ -5,6 +5,42 @@
 #include <iostream>
 #include "HelperT.h"
 
+using TxID = std::string;
+
+// Represents an out point - reference to a specific transaction output
+struct OutPoint
+{
+	TxID txid;		// ID (hash) of the transaction
+	uint32_t index;	// Index of the output in that transaction
+
+	bool operator==(const OutPoint& other) const
+	{
+		return (txid == other.txid && index == other.index);
+	}
+};
+
+// A custom hash for OutPoint so it can be used in unordered map
+struct OutPointHash
+{
+	std::size_t operator()(const OutPoint& op) const
+	{
+		std::hash<std::string> str_hash;
+		return (str_hash(op.txid) ^ (std::hash<uint32_t>()(op.index) << 1));
+	}
+};
+
+// A transaction output (what become UTXO if unspent)
+struct TxOutput
+{
+	uint64_t value;
+	std::string scriptPubKey;
+};
+
+struct TxInput
+{
+	OutPoint previousOutPoint; // Which UTXO this input is spending
+	std::string scriptSig;	   
+};
 
 class Transaction
 {
@@ -20,12 +56,16 @@ public:
 	std::string getReceiver() const { return _receiver; }
 	double getAmount() const { return _amount; }
 private:
-	std::string _sender;
-	std::string _receiver;
-	double _amount;
+	//std::string _sender;
+	//std::string _receiver;
+	//double _amount;
+	//time_t _timestamp;
+	//std::string _transactionID;
+
+	std::vector<TxInput> inputs;
+	std::vector<TxOutput> outputs;
 	time_t _timestamp;
 	std::string _transactionID;
-
 
 	std::string generateTransactionID();
 };
--- Blockchain_Project/Wallet.cpp
+++ Blockchain_Project/Wallet.cpp
@@ -6,3 +6,52 @@ void Wallet::InitializeTransaction(std::string k2, double amt)
 	Blockchain* chain = Blockchain::getInstance();
 	chain->addTransaction(trans);
 }
+
+Wallet::Wallet()
+{
+	/*
+	Generate random seed
+	get private key
+	calculate public key
+	calculate address
+	*/
+	Wallet(seed, true);
+}
+
+Wallet::Wallet(std::string seed, bool seedInitialize)
+{
+	/*
+	Get private key from seed
+	calculate public key
+	calculate address
+	*/
+
+	Wallet(privateKey);
+}
+
+Wallet::Wallet(std::string privateKey)
+{
+	/*
+	Calculate public key
+	calculate address
+	*/
+
+	cpp_int cppIntPublicKey = hexStringToCppInt(publicKey);
+	address = AddressGenerator::generateAddress(cppIntPublicKey);
+}
+
+std::string Wallet::cppIntToHexString(cpp_int v)
+{
+	std::string hexStr = v.str(16);
+	return hexStr;
+}
+
+cpp_int Wallet::hexStringToCppInt(std::string hex)
+{
+	// Remove "0x" prefix if present
+	if (hex.find("0x") == 0 || hex.find("0X") == 0) {
+		hex = hex.substr(2);
+	}
+
+	boost::multiprecision::cpp_int num(hex, 16);
+}
