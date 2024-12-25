0ec81a0c8ff8792d157b2427d16807cb7b4fd117
unknown <neslioz07@gmail.com>
2024-12-25 19:31:38 +0200

    Rebuilt transactions using UTXOs

--- Blockchain_Project/Transaction.cpp
+++ Blockchain_Project/Transaction.cpp
@@ -1,10 +1,9 @@
 #include "Transaction.h"
 
-Transaction::Transaction(const std::string& sender, const std::string receiver, double amount)
+Transaction::Transaction(std::vector<TxInput> inputs, std::vector<TxOutput> outputs)
 {
-	_sender = sender;
-	_receiver = receiver;
-	_amount = amount;
+	_inputs = inputs;
+	_outputs = outputs;
 	_timestamp = std::time(nullptr);
 	_transactionID = generateTransactionID();
 }
@@ -12,10 +11,21 @@ Transaction::Transaction(const std::string& sender, const std::string receiver, 
 void Transaction::displayTransaction() const
 {
 	std::string time = HelperT::timeToStr(_timestamp);
+	std::cout << "Transaction ID: " << _transactionID << std::endl 
+		<< "Time stamp: " << time << std::endl << std::endl;
 
-	std::cout << "Transaction ID: " << _transactionID << std::endl;
-	std::cout << _sender << " -> " << _receiver << " : " << _amount << std::endl;
-	std::cout << "Timestamp: " << time << std::endl;
+	std::cout << "Inputs: " << std::endl;
+	for (auto input : _inputs)
+	{
+		std::cout << input << std::endl;
+	}
+	std::cout << "Outputs: " << std::endl;
+	for (auto output : _outputs)
+	{
+		std::cout << output << std::endl;
+	}
+
+	std::cout << std::endl;
 }
 
 std::string Transaction::getTransactionID() const
@@ -23,10 +33,44 @@ std::string Transaction::getTransactionID() const
 	return _transactionID;
 }
 
-// TODO: Change to use hash functions
 std::string Transaction::generateTransactionID()
 {
-	return _sender + _receiver + std::to_string(_amount) + std::to_string(_timestamp);
+	std::stringstream ss;
+
+	// Include all transasction data in ss
+	for (auto input : _inputs)
+	{
+		ss << input;
+	}
+	for (auto output : _outputs)
+	{
+		ss << output;
+	}
+	ss << _timestamp;
+
+
+
+	// Hash the data to create a 16 bytes transaction ID
+
+	// Convert ss into uint8_t*
+	std::string strTx = ss.str();
+	uint8_t* buffer = new uint8_t[strTx.size()];
+	std::memcpy(buffer, strTx.data(), strTx.size());
+
+	// Create output hash buffer
+	uint8_t* hashedID = new uint8_t[TRANSACTION_ID_LENGTH];
+
+	// Hash the transaction data
+	Blake2b::hash(buffer, strTx.size(), hashedID, TRANSACTION_ID_LENGTH);
+
+	//  Convert transaction ID to hex string
+	std::string transactionID = Blake2b::bytesToHex(hashedID, TRANSACTION_ID_LENGTH);
+
+	// Clear memory used
+	delete[] buffer;
+	delete[] hashedID;
+
+	return transactionID;
 }
 
 
--- Blockchain_Project/Transaction.h
+++ Blockchain_Project/Transaction.h
@@ -4,6 +4,9 @@
 #include <ctime>
 #include <iostream>
 #include "HelperT.h"
+#include "Encryptions/Blake2b.h"
+
+#define TRANSACTION_ID_LENGTH 16 // 16 bytes
 
 using TxID = std::string;
 
@@ -36,37 +39,71 @@ struct TxOutput
 	std::string scriptPubKey;
 };
 
+
 struct TxInput
 {
 	OutPoint previousOutPoint; // Which UTXO this input is spending
 	std::string scriptSig;	   
+
 };
+
+// Print OutPoint
+std::ostream& operator<<(std::ostream& os, OutPoint op)
+{
+	os << "OutPoint(txid: " << op.txid << ", index: " << op.index << ")";
+	return os;
+}
+
+// Print TxInput
+std::ostream& operator<<(std::ostream& os, TxInput input) {
+	os << "TxInput(";
+	os << "previousOutpoint: " << input.previousOutPoint << ", ";
+	os << "scriptSig: " << input.scriptSig;
+	os << ")";
+	return os;
+}
+
+// Print TxOutput
+std::ostream& operator<<(std::ostream& os, const TxOutput& output) {
+	os << "TxOutput(";
+	os << "value: " << output.value << ", ";
+	os << "scriptPubKey: " << output.scriptPubKey;
+	os << ")";
+	return os;
+}
 
 class Transaction
 {
 public:
-	Transaction(const std::string& sender, const std::string receiver, double amount);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="inputs">Vector of input transactions to use</param>
+	/// <param name="outputs">Vector of output transactions to create</param>
+	Transaction(std::vector<TxInput> inputs, std::vector<TxOutput> outputs);
 	~Transaction() {};
 
+	/// <summary>
+	/// Display transaction
+	/// </summary>
 	void displayTransaction() const;
 
+	/// <summary>
+	/// Get transaction ID
+	/// </summary>
+	/// <returns>Transaction ID</returns>
 	std::string getTransactionID() const;
 
-	std::string getSender() const { return _sender; }
-	std::string getReceiver() const { return _receiver; }
-	double getAmount() const { return _amount; }
 private:
-	//std::string _sender;
-	//std::string _receiver;
-	//double _amount;
-	//time_t _timestamp;
-	//std::string _transactionID;
-
-	std::vector<TxInput> inputs;
-	std::vector<TxOutput> outputs;
+	std::vector<TxInput> _inputs;
+	std::vector<TxOutput> _outputs;
 	time_t _timestamp;
 	std::string _transactionID;
 
+	/// <summary>
+	/// Generates the transaction ID
+	/// </summary>
+	/// <returns>Transaction ID</returns>
 	std::string generateTransactionID();
 };
 
